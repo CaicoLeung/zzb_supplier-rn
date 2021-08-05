2a386a366f376e2153181fe83207a4b1029954a7
CaicoLeung <lkk1053190902@gmail.com>
2021-08-05 16:42:01 +0800

    feat: 商品管理List

--- screens/Goods/List.tsx
+++ screens/Goods/List.tsx
@@ -5,15 +5,13 @@ import { ListView, SearchBar, Tabs } from "@ant-design/react-native"
 import { SupGoodsTabs } from "@/helpers/goods"
 import { TabData } from "@ant-design/react-native/lib/tabs/PropsType"
 import { Goods } from "@/types/goods"
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
 import { ParamListBase } from "@react-navigation/routers"
+import { GoodsCart } from "@/components/GoodsCart"
 
 interface ParamList extends ParamListBase {
   [SupGoodsTabs.售卖中]: undefined
   [SupGoodsTabs.已下架]: undefined
 }
-
-// const Tabs = createMaterialTopTabNavigator<ParamList>()
 
 export default function GoodsList() {
   const tabs = [{ title: SupGoodsTabs.售卖中 }, { title: SupGoodsTabs.已下架 }]
@@ -26,7 +24,51 @@ export default function GoodsList() {
     try {
       const pageLimit = 30
       const skip = (currentPage - 1) * pageLimit
-      let rowData = Array.from({ length: pageLimit }, (_, index) => `item -> ${index + skip}`)
+      let rowData: Goods.SupGoods[] = Array.from({ length: pageLimit }, (_, index) => ({
+        id: index + skip,
+        goods_no: "AUR10044",
+        name: "水滴型翡翠帝王绿豆豆，编辑后的商品0713🥔加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长加长" + (index + skip),
+        description: "",
+        images: "[2826]",
+        user_id: 332,
+        status: 1,
+        cate_str: "黄金",
+        style: "戒指",
+        origin_code: "08020921",
+        price_cost: 1728381.472,
+        stock_num: 10,
+        sur_num: 0,
+        add_goods_cycle: "7-15天",
+        create_time: "2021-08-02T09:22:18Z",
+        update_time: "2021-08-02T09:22:18Z",
+        cover: "https://oss.z.vip/BwyxApi/static/upload/202108/02/1627867310131571121.jpg",
+        opt_user_id: 332,
+        cate_id: 1,
+        style_id: 0,
+        weight: 1,
+        price: 0,
+        is_sale: 1,
+        rel_id: 0,
+        process: 1,
+        remark: "[]",
+        pictures: [
+          {
+            id: 2826,
+            type: "image",
+            url: "https://oss.z.vip/BwyxApi/static/upload/202108/02/1627867310131571121.jpg",
+            cover: "https://oss.z.vip/BwyxApi/static/upload/202108/02/1627867310131571121.jpg",
+            file_info: '{"height":3024,"orientation":"up","width":4032}',
+            file: "static/upload/202108/02/1627867310131571121.jpg",
+            source: "0",
+            source_id: 0,
+            rate: "",
+            title: "",
+            sort: 0,
+            create_time: "2021-08-02T09:21:51Z",
+            update_time: "2021-08-02T09:21:51Z",
+          },
+        ],
+      }))
       if (currentPage === 3) {
         rowData = []
       }
@@ -38,29 +80,23 @@ export default function GoodsList() {
   }
 
   const keyExtractor = (item: Goods.SupGoods, index: number) => {
-    return `goods-${item.id}-${index}`
+    return `${item.id}-${index}`
   }
 
-  const renderItem = (item: Goods.SupGoods) => {
-    return (
-      <View style={{ padding: 10 }}>
-        <Text>{item}</Text>
-      </View>
-    )
-  }
+  const renderItem = (item: Goods.SupGoods) => (
+    <GoodsCart cover={item.cover} name={item.name} desc={`销量: ${item.sur_num}`} tags={[item.cate_str, item.style]} num={`现货库存: ${item.stock_num}`} price={item.price_cost} />
+  )
 
   return (
     <View style={styles.container}>
       <SearchBar placeholder="请输入搜索商品名称、编号" />
       <Tabs tabs={tabs} animated>
-        <ScrollView>
-          <ListView onFetch={onFetch} keyExtractor={keyExtractor} renderItem={renderItem} numColumns={1} />
-        </ScrollView>
-        <ScrollView>
-          <View>
-            <Text>121212121</Text>
-          </View>
-        </ScrollView>
+        <View>
+          <ListView<Goods.SupGoods> onFetch={onFetch} keyExtractor={keyExtractor} renderItem={renderItem} numColumns={1} />
+        </View>
+        <View>
+          <Text>121212121</Text>
+        </View>
       </Tabs>
     </View>
   )
