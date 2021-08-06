004b55776b9b3decdc2d1be4133863221b6ae2fb
CaicoLeung <lkk1053190902@gmail.com>
2021-08-06 14:23:20 +0800

    fix: 重构Tabs实现, 自定义样式

--- screens/Goods/List.tsx
+++ screens/Goods/List.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { View } from "@/components/Themed"
-import { Modal, ScrollView, StyleSheet, Text } from "react-native"
-import { ListView, SearchBar, Tabs } from "@ant-design/react-native"
+import { Dimensions, Modal, ScrollView, StyleProp, StyleSheet, Text, ViewStyle } from "react-native"
+import { ListView, SearchBar } from "@ant-design/react-native"
 import { ShelfSupGoodsTab, SupGoodsTabs } from "@/helpers/goods"
 import { TabData } from "@ant-design/react-native/lib/tabs/PropsType"
 import { Goods } from "@/types/goods"
@@ -11,25 +11,23 @@ import { Upload } from "@/types/upload"
 import ImageViewer from "react-native-image-zoom-viewer"
 import { IImageInfo } from "react-native-image-zoom-viewer/built/image-viewer.type"
 import { getSupGoodsList } from "@/services/goods"
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import { NavigationContainer, RouteConfig } from "@react-navigation/native"
 
 interface ParamList extends ParamListBase {
   [SupGoodsTabs.售卖中]: undefined
   [SupGoodsTabs.已下架]: undefined
 }
 
-export default function GoodsList() {
-  const [keyword, setKeyword] = useState("")
-  const [visibleState, setVisibleState] = React.useState<{ visible: boolean; pictures: Upload.Image[] }>({ visible: false, pictures: [] })
-  const tabs = [{ title: SupGoodsTabs.售卖中 }, { title: SupGoodsTabs.已下架 }]
-  const onTabsChange = (tab: TabData, index: number) => {
-    console.log(tab, index)
-  }
+const Tabs = createMaterialTopTabNavigator()
 
+function ListViewRender() {
+  const [visibleState, setVisibleState] = React.useState<{ visible: boolean; pictures: Upload.Image[] }>({ visible: false, pictures: [] })
   const onFetch = async (currentPage: number, startFetch: (...args: any[]) => any, abortFetch: () => void) => {
     try {
       const pageLimit = 20
       const params: Goods.SupGoodsPageParams = {
-        keyword,
+        keyword: "",
         tab: ShelfSupGoodsTab.上架,
         current: currentPage,
         pageSize: pageLimit,
@@ -70,18 +68,9 @@ export default function GoodsList() {
       },
     }))
   }
-
   return (
-    <View style={styles.container}>
-      <SearchBar placeholder="请输入搜索商品名称、编号" />
-      <Tabs tabs={tabs} animated onChange={onTabsChange}>
-        <View>
-          <ListView<Goods.SupGoods<Upload.Image[]>> onFetch={onFetch} keyExtractor={keyExtractor} renderItem={renderItem} numColumns={1} />
-        </View>
-        <View>
-          <Text>121212121</Text>
-        </View>
-      </Tabs>
+    <View style={{ flex: 1 }}>
+      <ListView<Goods.SupGoods<Upload.Image[]>> onFetch={onFetch} keyExtractor={keyExtractor} renderItem={renderItem} numColumns={1} />
       <Modal visible={visibleState.visible} transparent>
         <ImageViewer
           imageUrls={formatToImageUrls(visibleState.pictures)}
@@ -93,10 +82,48 @@ export default function GoodsList() {
   )
 }
 
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>A看过我</Text>
+        <Text>B看过我</Text>
+      </View>
+    )
+  }
+}
+
+export default function GoodsList() {
+  const [keyword, setKeyword] = useState("")
+
+  const getInicatoeStyle = (width: number): StyleProp<ViewStyle> => {
+    return {
+      width,
+      left: (Dimensions.get("window").width / 2 - width) / 2,
+      backgroundColor: "#1989fa",
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <SearchBar placeholder="请输入搜索商品名称、编号" value={keyword} onChange={setKeyword} />
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#1989fa",
+          tabBarLabelStyle: { fontSize: 16, fontWeight: "600" },
+          tabBarIndicatorStyle: getInicatoeStyle(50),
+        }}
+      >
+        <Tabs.Screen name={SupGoodsTabs.售卖中} component={ListViewRender} />
+        <Tabs.Screen name={SupGoodsTabs.已下架} component={ListViewRender} />
+      </Tabs.Navigator>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
   },
   scrollView: {
     flex: 1,
